68aba5ac5ea58440e2cf267fca082549ac4b267f
Stephen Z <mayistep@icloud.com>
2020-07-24 21:16:17 -0400

    final push to heroku

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,12 +1,10 @@
 var counter = 0;
 function changeBG(){
     var imgs = [
-        "url(https://images.unsplash.com/photo-1531097517181-3de20fd3f05c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)",
-        "url(https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2295&q=80)",
-        "url(https://images.unsplash.com/photo-1528150206408-07a3f3025282?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)",
-        "url(https://images.unsplash.com/photo-1535846417087-ff0057791ecf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2632&q=80)",
-        "url(https://images.unsplash.com/photo-1526011881888-8dba3f788ede?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)"
-       
+      "url(https://images.unsplash.com/photo-1531097517181-3de20fd3f05c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)",
+      "url(https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2295&q=80)",
+      "url(https://images.unsplash.com/photo-1528150206408-07a3f3025282?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)",
+      "url(https://images.unsplash.com/photo-1535846417087-ff0057791ecf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2632&q=80)",
       ]
     
     if(counter === imgs.length) counter = 0;
@@ -15,6 +13,6 @@ function changeBG(){
     counter++;
 }
   
-  setInterval(changeBG, 3000);
+  setInterval(changeBG, 2000);
 
 
